180b77743df1b2b66677d8caccdc93d7901cb004
bellabrillantes <78109610+bellabrillantes@users.noreply.github.com>
2021-03-07 05:25:59 +0800

    Add files via upload

---
+++ brandnav/hamburger.js
@@ -0,0 +1,11 @@
+function showmenu(){
+
+  document.getElementById('hamburger-overlay').style.display="block";
+
+}
+
+function hidemenu(){
+
+  document.getElementById('hamburger-overlay').style.display="none";
+
+}
